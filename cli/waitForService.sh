41fe911ed70aa7015501a7db03dde9da856c4919
maxsun <max@bedrockocean.com>
2023-06-11 02:26:30 -0400

    working on cli

--- cli/waitForService.sh
+++ cli/waitForService.sh
@@ -6,8 +6,8 @@ COUNT=0
 R="$(dig @"$1" -p "$2" "$3" +noall +nocmd +answer | sed 's/\t/ /g' | rev | cut -d ' ' -f 1 | rev | tail -n 1)"
 
 echo "!"
-until [ "$R" != "+cmd" ]; do
-    printf "Waiting for [%s] (attempt %s)" "$3" "$COUNT" 
+until [ "$R" != "+cmd" ] && [ ! -z "$R" ]; do
+    printf "Waiting for [%s] (attempt %s)" "$3" "$COUNT"
     COUNT=$((COUNT+1))
 
     if [ ${COUNT} -gt ${MAX_ATTEMPTS} ]; then
