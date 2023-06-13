c9a0307fa4ec90236bcd760477ce41f610859df2
maxsun <max@bedrockocean.com>
2023-06-13 16:31:12 -0400

    tests

---
+++ list_services.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+i=0
+
+pname=""
+address=""
+port=""
+ip=""
+
+ADVERTISE_IP="$(tailscale ip  --1)"
+
+echo "network ip: $ADVERTISE_IP"
+
+var="$(etcdctl get "$ADVERTISE_IP" --prefix)"
+while read -r line
+do
+    # lsof output without extra-spacing
+    # x="$(echo "$line" | sed 's/ * / /g')"
+    if ((i % 2 == 0)); then
+        # echo "$i $line"
+        vpnIpAndAddress="$line"
+        ip="$(echo "$vpnIpAndAddress" | cut -d '/' -f 1)"
+        addressAndPort="$(echo "$vpnIpAndAddress" | cut -d '/' -f 2-)"
+        address="$(echo "$addressAndPort" | cut -d ':' -f 1)"
+        port="$(echo "$addressAndPort" | cut -d ':' -f 2)"
+
+    else
+        pname="$line"
+        echo "$pname $ip $address $port"
+    fi
+    i=$((i + 1))
+
+done < <(printf '%s\n' "$var")
---
+++ netdetails.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+ADVERTISE_IP="$(tailscale ip  --1)"
+
+echo "network ip: $ADVERTISE_IP"
+
+etcdctl del "$ADVERTISE_IP" --prefix
+
+node_hostname="$(uname -n)"
+
+echo "Registering services for $node_hostname"
+
+var="$(lsof -V -Pi -n +c 0 | grep "(LISTEN)")"
+while read -r line
+do
+    # lsof output without extra-spacing
+    x="$(echo "$line" | sed 's/ * / /g')"
+    # command name
+    pname="$(echo "$x" | cut -d ' ' -f 1)"
+    pid="$(echo "$x" | cut -d ' ' -f 2)"
+    address="$(echo "$x" | cut -d ' ' -f 9)"
+
+
+    # x="$(echo "$line" | sed 's/\s+/ /' | cut -d ' ' -f 1)"
+    # y="$(echo "$line" | sed 's/\ +/\ /' | cut -d ' ' -f 2)"
+    # echo "$line"
+    echo "$pid - $pname - $address"
+
+    # use `ps` 
+    stats="$(ps aux $pid)"
+    stats="$(echo "$stats" | grep $pid | sed 's/ * / /g')"
+
+    cpu="$(echo "$stats" | cut -d ' ' -f 3)"
+    mem="$(echo "$stats" | cut -d ' ' -f 4)"
+    cmd="$(echo "$stats" | cut -d ' ' -f 11-)"
+
+    etcdctl put "$ADVERTISE_IP/$address" "$pname" 
+
+done < <(printf '%s\n' "$var")
